0bc5cc06453c7d7dd3c719f213c078afb1e89f28
hanswys <123027447+hanswys@users.noreply.github.com>
2025-02-21 13:10:49 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'hanswys/hiredai-webapp'
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
